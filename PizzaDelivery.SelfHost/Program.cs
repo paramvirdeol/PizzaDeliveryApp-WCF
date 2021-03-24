94977eb5d377bdcf0900869312c5eabd6777cf9e
paramvir.deol@gmail.com <paramvir.deol@gmail.com>
2021-03-24 16:08:30 +0530

    Added self-hosting

---
+++ PizzaDelivery.SelfHost/Program.cs
@@ -0,0 +1,31 @@
+﻿using PizzaDelivery.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaDelivery.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var host = new ServiceHost(typeof(PizzaService));
+                host.Open();
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadLine();
+                host.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
